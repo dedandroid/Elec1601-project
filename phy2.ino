47be3e40172fc3cabc9d8481aab1aadd28b081b7
Ian Chang <ycha5975@uni.sydney.edu.au>
2023-10-23 14:03:25 +1100

    working pwm valesu

--- phy2.ino
+++ phy2.ino
@@ -1,5 +1,19 @@
-//#include <Arduino.h>
 #include <Servo.h>
+#include <math.h>
+#include <Arduino.h>
+
+struct Robot {
+    int x, y;
+    double true_x, true_y;
+    int direction;
+    int angle;
+    int currentSpeed;
+    int width, height;
+    int auto_mode;
+};
+
+Robot myRobot;
+
 
 Servo servoLeft;
 Servo servoRight;
@@ -16,61 +30,95 @@ const int CENTER_IR_LED_PIN = 6;
 const int CENTER_IR_SENSOR_PIN = 7;
 const int CENTER_RED_LED_PIN = A1;
 
-struct Robot {
-    int x, y;
-    double true_x, true_y;
-    int direction;
-    int angle;
-    int currentSpeed;
-    int width, height;
-    int auto_mode;
-};
+void setup()                                 // Built-in initialization block
+{
+   servoLeft.attach(13);
+    servoRight.attach(12);
 
-Robot myRobot;
+    pinMode(LEFT_IR_LED_PIN, OUTPUT);
+    pinMode(LEFT_IR_SENSOR_PIN, INPUT);
+    pinMode(LEFT_RED_LED_PIN, OUTPUT);
 
-void setup() {
-    pinMode(irReceiverPin, INPUT);  // IR receiver pin is an input
-    pinMode(irLedPin, OUTPUT);  // IR LED pin is an ouput
-    pinMode(redLedPin, OUTPUT);  // Red LED pin is an output
-    Serial.begin(9600);  // Set data rate to 9600 bps
-}
+    pinMode(RIGHT_IR_LED_PIN, OUTPUT);
+    pinMode(RIGHT_IR_SENSOR_PIN, INPUT);
+    pinMode(RIGHT_RED_LED_PIN, OUTPUT);
 
-void loop() {
-    int leftDistance = irDistance(LEFT_IR_LED_PIN, LEFT_IR_SENSOR_PIN);
-    int rightDistance = irDistance(RIGHT_IR_LED_PIN, RIGHT_IR_SENSOR_PIN);
-    int centerDistance = irDistance(CENTER_IR_LED_PIN, CENTER_IR_SENSOR_PIN);
+    pinMode(CENTER_IR_LED_PIN, OUTPUT);
+    pinMode(CENTER_IR_SENSOR_PIN, INPUT);
+    pinMode(CENTER_RED_LED_PIN, OUTPUT);
 
-    // Debugging print
-    Serial.print("Left: ");
-    Serial.print(leftDistance);
-    Serial.print(" | Center: ");
-    Serial.print(centerDistance);
-    Serial.print(" | Right: ");
-    Serial.println(rightDistance);
+   Serial.begin(9600);                       // Set data rate to 9600 bps
+}  
+ 
+void loop()                                  // Main loop auto-repeats
+{
+  int LeftirVal = irDetect(LEFT_IR_LED_PIN, LEFT_IR_SENSOR_PIN, 42000);               // Check for Left
+  int leftDist = irDistance(LEFT_IR_LED_PIN, LEFT_IR_SENSOR_PIN);
+  Serial.print("Left : ");
+  Serial.println(leftDist);                     // Display 1/0 no detect/detect
 
-    // Run the navigation function
-    robotAutoMotorMove(&myRobot, centerDistance, leftDistance, rightDistance);
+  int RightirVal = irDetect(RIGHT_IR_LED_PIN, RIGHT_IR_SENSOR_PIN, 42000);               // Check for Left
+  int rightDist = irDistance(RIGHT_IR_LED_PIN, RIGHT_IR_SENSOR_PIN);
+  Serial.print("Right : ");
+  Serial.println(rightDist);                     // Display 1/0 no detect/detect
 
+  int CenterirVal = irDetect(CENTER_IR_LED_PIN, CENTER_IR_SENSOR_PIN, 42000);               // Check for Left
+  int centerDist = irDistance(CENTER_IR_LED_PIN, CENTER_IR_SENSOR_PIN);
+  Serial.print("Center : ");
+  Serial.println(centerDist);                     // Display 1/0 no detect/detect
+
+  // Run the navigation function
+  robotAutoMotorMove(&myRobot, centerDist, leftDist, rightDist);
     // Implement the motor control based on the navigation decisions
-    controlMotorsBasedOnNavigation(&myRobot);
+  controlMotorsBasedOnNavigation(&myRobot);
+
+
+  if (LeftirVal == 0)                            // Optional - display detection by setting red LED high
+  {
+    digitalWrite(LEFT_RED_LED_PIN, HIGH);             
+  } else{
+    digitalWrite(LEFT_RED_LED_PIN, LOW);
+  }
+  
+  
+  if (RightirVal == 0)                            // Optional - display detection by setting red LED high
+  {
+    digitalWrite(RIGHT_RED_LED_PIN, HIGH);             
+  } else{
+    digitalWrite(RIGHT_RED_LED_PIN, LOW);
+  }
+
+  if (CenterirVal == 0)                            // Optional - display detection by setting red LED high
+  {
+    digitalWrite(CENTER_RED_LED_PIN, HIGH);             
+  } else{
+    digitalWrite(CENTER_RED_LED_PIN, LOW);
+  }
+
+  delay(500);                                // 0.5 second delay - just long enough to see the LED blink
 }
 
-#include <math.h>
-#define UP 1
-#define DOWN 2
-#define LEFT 3
-#define RIGHT 4
-#define CONSTANT_SPEED 10
+#define UP 2 
+#define CONSTANT_SPEED 5
 #define ADJUSTMENT_SPEED 1  // Speed when adjusting
 #define TURN_SPEED 1
-#define MAX_SMOOTH_TURN_ANGLE 10 // Adjust as per testing
-#define ADJUSTMENT_ANGLE 10  
-#define TURN_DELAY_CYCLES 3  // Number of cycles to wait before initiating a turn
+#define MAX_SMOOTH_TURN_ANGLE 8 // Adjust as per testing
+#define ADJUSTMENT_ANGLE 5  
+#define TURN_DELAY_CYCLES 1  // Number of cycles to wait before initiating a turn
 #define DEAD_ZONE 3  // Angle misalignment below which corrections are not made
 #define max(a,b) ((a) > (b) ? (a) : (b))
 #define min(a,b) ((a) < (b) ? (a) : (b))
-#define SMOOTHING_ANGLE 5  // Smaller angle for smoother adjustments
+#define SMOOTHING_ANGLE 3  // Smaller angle for smoother adjustments
 #define TURN_BASE 2  // Base for exponential increase in turn angle
+
+void setSpeed(struct Robot* robot, int desiredSpeed) {
+    if (robot->currentSpeed < desiredSpeed) {
+        robot->currentSpeed += 1;
+    } else if (robot->currentSpeed > desiredSpeed) {
+        robot->currentSpeed -= 1;
+    }
+}
+
 
 void robotAutoMotorMove(struct Robot * robot, int front_centre_sensor, int left_sensor, int right_sensor) {
     static int adjustment_made = 0; 
@@ -82,27 +130,87 @@ void robotAutoMotorMove(struct Robot * robot, int front_centre_sensor, int left_
     static int turning_right = 0;  // Flag to indicate whether the robot is turning right
     static int was_adjusted = 0;  // Flag to indicate if the robot was adjusted in the previous cycle
     static int is_completing_turn = 0;  // Flag to indicate if the robot is in the middle of completing a turn
+    static int turning_back = 0; 
     
+    // If the robot is completing a turn, ignore all sensors and finish the turn
+    if (is_completing_turn) {
+        printf("Completing a turn...\n");
+        int remaining_angle = 90 - total_turn_angle;  
+        robot->angle += turning_left ? -remaining_angle : remaining_angle;  
+        total_turn_angle = 0;
+        turning_left = turning_right = 0;
+        is_completing_turn = 0;
+        return;
+    }
+        
+    // If the front center sensor detects a wall
+    if (front_centre_sensor != 0) {
+        printf("Wall detected in front!\n");
+        setSpeed(robot, 0);  // Ensure the robot doesn't move forward
+        
+        // When encountering a dead end, perform a 180-degree turn in 15-degree increments
+        if (left_sensor != 0 && right_sensor != 0) {
+            printf("Dead end encountered, turning back...\n");
+            turning_back = 1;
+            turning_left = turning_right = 0;  // Ensure other turning flags are off
+        }
+        
+        // Logic to continue the 180-degree turn once initiated
+        if (turning_back) {
+            if (total_turn_angle < 180) {
+                printf("Continuing back turn...\n");
+                robot->angle -= 15;  
+                total_turn_angle += 15;
+            } else {
+                // Reset variables after the turn is complete
+                printf("Back turn completed.\n");
+                turning_back = 0;
+                total_turn_angle = 0;
+            }
+            return;  // Exit function to ignore other logic while turning back
+        }
+        
+        // New logic for initiating a turn when a wall is detected:
+        if (right_sensor == 0) {
+            printf("Turning right (wall in front, no wall to right)...\n");
+            robot->angle += 15;  // Make a 90-degree right turn
+            return;  // Exit function after initiating the turn
+        }
+        else if (left_sensor == 0) {
+            printf("Turning left (wall in front, no wall to left)...\n");
+            robot->angle -= 15;  // Make a 90-degree left turn
+            return;  // Exit function after initiating the turn
+        }
+
+        // Complete the turn if already in progress
+        if (turning_left || turning_right) {
+            printf("Completing a turn (wall in front, turning left/right)...\n");
+            is_completing_turn = 1;  // Set the flag to ignore sensors until the turn is complete
+            return;
+        }
+    }
+
 
 
     // If the front center sensor doesn't detect a wall, continue moving forward
     else {
-
+        printf("No wall in front, moving forward...\n");
         robot->direction = UP;
-        robot->currentSpeed = CONSTANT_SPEED; 
+        setSpeed(robot, CONSTANT_SPEED);
 
         // If the robot is not centered and not currently turning, adjust the angle
         if (adjustment_made != 0 && !turning_left && !turning_right) {
-
+            printf("Adjusting angle (not centered, not turning)...\n");
             robot->angle -= adjustment_made;  
             adjustment_made = 0;  
         }
 
         // Logic for initiating a gradual left turn
         if (left_sensor == 0 && !turning_right) {
-
+            printf("Initiating gradual left turn...\n");
             turning_left = 1; 
-            robot->currentSpeed = TURN_SPEED;
+            setSpeed(robot, TURN_SPEED);
+
             if (total_turn_angle < 90) {
                 // Linear increase for slower ramp-up
                 int dynamic_smooth_turn_angle = MAX_SMOOTH_TURN_ANGLE + (0.1 * total_turn_angle);  
@@ -119,9 +227,10 @@ void robotAutoMotorMove(struct Robot * robot, int front_centre_sensor, int left_
         }
         // Logic for initiating a gradual right turn
         else if (right_sensor == 0 && !turning_left) {
-
+            printf("Initiating gradual right turn...\n");
             turning_right = 1;  
-            robot->currentSpeed = TURN_SPEED;
+            setSpeed(robot, TURN_SPEED);
+
             if (total_turn_angle < 90) {
                 // Linear increase for slower ramp-up
                 int dynamic_smooth_turn_angle = MAX_SMOOTH_TURN_ANGLE + (0.1 * total_turn_angle);  
@@ -139,107 +248,117 @@ void robotAutoMotorMove(struct Robot * robot, int front_centre_sensor, int left_
 
         // Stick to the wall logic
         if (left_sensor > 3 && left_sensor <= 5 && !was_adjusted) {
-
+            printf("Adjusting left (left sensor: %d)...\n", left_sensor);
             robot->angle += ADJUSTMENT_ANGLE;
-            robot->currentSpeed = ADJUSTMENT_SPEED;  // Slow down when adjusting
+            setSpeed(robot, ADJUSTMENT_SPEED);  // Slow down when adjusting
             was_adjusted = 1;
         } 
         else if (right_sensor > 3 && right_sensor <= 5 && !was_adjusted) {
-
+            printf("Adjusting right (right sensor: %d)...\n", right_sensor);
             robot->angle -= ADJUSTMENT_ANGLE;
-            robot->currentSpeed = ADJUSTMENT_SPEED;  // Slow down when adjusting
+            setSpeed(robot, ADJUSTMENT_SPEED);  // Slow down when adjusting
             was_adjusted = 1;
         } 
         else if (left_sensor == 2 && !was_adjusted) {
-
+            printf("Smoothing left (left sensor: 2)...\n");
             robot->angle -= SMOOTHING_ANGLE;
-            robot->currentSpeed = ADJUSTMENT_SPEED;  // Slow down when adjusting
+            setSpeed(robot, ADJUSTMENT_SPEED);  // Slow down when adjusting
             was_adjusted = 1;
         } 
         else if (right_sensor == 2 && !was_adjusted) {
-
+            printf("Smoothing right (right sensor: 2)...\n");
             robot->angle += SMOOTHING_ANGLE;
-            robot->currentSpeed = ADJUSTMENT_SPEED;  // Slow down when adjusting
+            setSpeed(robot, ADJUSTMENT_SPEED);  // Slow down when adjusting
             was_adjusted = 1;
         } 
         else if (left_sensor == 3 && !was_adjusted) {
-
+            printf("Smoothing left (left sensor: 3)...\n");
             robot->angle += SMOOTHING_ANGLE;
-            robot->currentSpeed = ADJUSTMENT_SPEED;  // Slow down when adjusting
+            setSpeed(robot, ADJUSTMENT_SPEED);  // Slow down when adjusting
             was_adjusted = 1;
         } 
         else if (right_sensor == 3 && !was_adjusted) {
-
+            printf("Smoothing right (right sensor: 3)...\n");
             robot->angle -= SMOOTHING_ANGLE;
-            robot->currentSpeed = ADJUSTMENT_SPEED;  // Slow down when adjusting
+            setSpeed(robot, ADJUSTMENT_SPEED);  // Slow down when adjusting
             was_adjusted = 1;
         }
         else {
-
+            printf("No adjustment needed, moving forward...\n");
             was_adjusted = 0;  // Reset adjustment flag
-            robot->currentSpeed = CONSTANT_SPEED;  // Ensure to revert back to normal speed
+            setSpeed(robot, CONSTANT_SPEED);  // Ensure to revert back to normal speed
         }
+    // Output the robot's angle and speed after each iteration
+    printf("Robot's angle: %d | Robot's speed: %d\n", robot->angle, robot->currentSpeed);
     }
-
 }
 
-#define SERVO_STOP 90
-#define SERVO_MAX_FORWARD 180
-#define SERVO_MAX_BACKWARD 0
+#define PULSE_STOP 1500
+#define PULSE_MAX_FORWARD 1700
+#define PULSE_MAX_BACKWARD 1300
 
 #define MAX_TURN_SPEED_DIFFERENCE 50  // Max speed difference between wheels during a turn
 
-void controlMotorsBasedOnNavigation(Robot* robot) {
-    int leftServoSpeed = SERVO_STOP;
-    int rightServoSpeed = SERVO_STOP;
+void controlMotorsBasedOnNavigation(struct Robot* robot) {
+    Serial.println(robot);
 
-    // Basic Movement: 
-    // Map the speed [-1, 1] to servo command [SERVO_MAX_BACKWARD, SERVO_MAX_FORWARD]
-    if(robot->currentSpeed != 0) {
-        leftServoSpeed = map(robot->currentSpeed, -1, 1, SERVO_MAX_BACKWARD, SERVO_MAX_FORWARD);
-        rightServoSpeed = map(robot->currentSpeed, -1, 1, SERVO_MAX_BACKWARD, SERVO_MAX_FORWARD);
-    }
+    int leftServoPulse = PULSE_STOP;
+    int rightServoPulse = PULSE_STOP;
+
+    // Map the robot's current speed to the appropriate pulse widths
+    leftServoPulse = map(robot->currentSpeed, -1, 1, PULSE_STOP, PULSE_MAX_FORWARD);
+    rightServoPulse = map(robot->currentSpeed, -1, 1, PULSE_MAX_FORWARD, PULSE_STOP);
+
+    Serial.print("Left servo pulse: ");
+    Serial.println(leftServoPulse);
+    Serial.print("Right servo pulse: ");
+    Serial.println(rightServoPulse);
 
     // Turning adjustments while moving:
-    if(robot->currentSpeed != 0) {
+    if(robot->currentSpeed > 0) {
         int turnAdjustment = map(abs(robot->angle), 0, 90, 0, MAX_TURN_SPEED_DIFFERENCE);
+        Serial.print("Turn adjustment: ");
+        Serial.println(turnAdjustment);
         if(robot->angle > 0) {
             // Right Turn: Slow down the right wheel
-            rightServoSpeed = constrain(rightServoSpeed - turnAdjustment, SERVO_MAX_BACKWARD, SERVO_MAX_FORWARD);
+            rightServoPulse = min(rightServoPulse + turnAdjustment, PULSE_MAX_FORWARD);
         } else if(robot->angle < 0) {
             // Left Turn: Slow down the left wheel
-            leftServoSpeed = constrain(leftServoSpeed - turnAdjustment, SERVO_MAX_BACKWARD, SERVO_MAX_FORWARD);
+            leftServoPulse = max(leftServoPulse - turnAdjustment, PULSE_STOP);
         }
     }
 
-    // Write the determined values to the servos
-    servoLeft.write(leftServoSpeed);
-    servoRight.write(rightServoSpeed);
-}
-
-int irDetect(long frequency) {
-    tone(irLedPin, frequency);  // Turn on the IR LED square wave
-    delay(1);  // Wait 1 ms
-    noTone(irLedPin);  // Turn off the IR LED
-    int ir = digitalRead(irReceiverPin);  // IR receiver -> ir variable
-    delay(1);  // Down time before recheck
-    return ir;  // Return 1 no detect, 0 detect
-}
-
-int irDistance(int irLedPin, int irReceiverPin) {
-    int distance = 0;
-    for(long f = 38000; f <= 42000; f += 1000) {
-        distance += irDetect(f);
-    }
-    // Map the accumulated distance (which goes from 0 to 5) to a discrete scale (0 - 5).
-    // Note: Actual mapping might need to be adjusted based on empirical testing.
-    int scaledDistance = map(distance, 0, 50, 0, 5);  // Assuming 50 is a max value
-    
-    // Ensure that the scaled distance is within the desired range [0, 5].
-    scaledDistance = constrain(scaledDistance, 0, 5);
-    
-    return scaledDistance;
+    // Write the determined pulse widths to the servos
+    servoLeft.writeMicroseconds(leftServoPulse);
+    servoRight.writeMicroseconds(rightServoPulse);
 }
 
 
+// IR Object Detection Function
 
+int irDetect(int irLedPin, int irReceiverPin, long frequency)
+{
+  tone(irLedPin, frequency);                 // Turn on the IR LED square wave
+  delay(1);                                  // Wait 1 ms
+  noTone(irLedPin);                          // Turn off the IR LED
+  int ir = digitalRead(irReceiverPin);       // IR receiver -> ir variable
+  delay(1);                                  // Down time before recheck
+
+  if (ir == 0){
+    return 1;
+  } else{
+    return 0;
+  }
+}
+
+// IR distance measurement function
+int irDistance(int irLedPin, int irReceivePin)
+{
+   int distance = 0;  
+   for(long f = 47000; f <= 50000; f += 1000)
+   {
+      int val = irDetect(irLedPin, irReceivePin, f);
+      distance += val;
+   }
+   return distance;
+}
